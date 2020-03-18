241a332222231f69036ce00c49c6c871146c1a15
Daniel Doblado <danieldoblado@gmail.com>
2020-03-18 13:15:20 +0100

    refactor: Simplify DownloadButton

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -5,39 +5,31 @@ export interface Props {
   waypoints: Waypoint[]
 }
 
-const DownloadButton = ({ waypoints }: Props) => (
-  <button
-    className="btn"
-    type="button"
-    onClick={() => {
-      const gpx = `
-      <?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
-      <gpx version="1.1" >
-        ${waypoints.map(({ lat, lng }, i) => (`
-          <wpt lat="${lat}" lon="${lng}">
-              <name>Waypoint ${i + 1}</name>
-          </wpt>`
-      ))}
-      <trk>
-        <name>Route</name>
-        <trkseg>
-        ${waypoints.map(({ lat, lng }) => (`<trkpt lat="${lat}" lon="${lng}"></trkpt>`))}
-        </trkseg>
-      </trk>
-      </gpx>`;
+const DownloadButton = ({ waypoints }: Props) => {
+  const gpx = `
+  <?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
+    <gpx version="1.1" >
+      ${waypoints.map(({ lat, lng }, i) => (`
+        <wpt lat="${lat}" lon="${lng}">
+            <name>Waypoint ${i + 1}</name>
+        </wpt>`))}
+    <trk>
+      <name>Route</name>
+      <trkseg>
+      ${waypoints.map(({ lat, lng }) => (`<trkpt lat="${lat}" lon="${lng}"></trkpt>`))}
+      </trkseg>
+    </trk>
+  </gpx>`;
 
-      // TODO: maybe use a ref instead of creating dom here
-      const link = document.createElement('a');
-      link.setAttribute('href', `data:text/gpx;charset=utf-8,${encodeURIComponent(gpx)}`);
-      link.setAttribute('download', `Waypoints ${new Date().toDateString()}.gpx`);
-      link.style.display = 'none';
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }}
-  >
-    Download your Route
-  </button>
-);
+  return (
+    <a
+      href={`data:text/xml;charset=utf-8,${encodeURIComponent(gpx)}`}
+      className="btn"
+      download={`Waypoints ${new Date().toDateString()}.gpx`}
+    >
+      Download your Route
+    </a>
+  );
+};
 
 export default DownloadButton;
